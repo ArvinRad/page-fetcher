f40339d5c173cd495e35bebc7cf6e484aadcc06c
ArvinRad <radanarvin@gmail.com>
2021-04-08 04:16:13 +0000

    Requesting page from a URL using Request

--- fetcher.js
+++ fetcher.js
@@ -2,12 +2,17 @@ const address = process.argv;
 const request = require('request');
 const fs = require('fs');
 let url = address[2];
-const myData = request(`${url}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log(body);
-});
-fs.writeFile(`${address[3]}`, myData, function (err) {
-    if (err) return console.log(err);
-    console.log(`Downloaded and saved 3261 bytes to ${address[3]}.`);
-  }); // Print the HTML to the index.html.
+request(`${url}`, (err, response, body) => {
+  if(err) {
+    console.log('error:', err); // Print the error if one occurred
+  } else {
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    fs.writeFile(`${address[3]}`, body, function (err) {
+      if (err) {
+        return console.log(err);
+      } else {
+        console.log(`Downloaded and saved 3261 bytes to ${address[3]}.`);
+      }
+    });
+  }
+  });
